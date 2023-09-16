a1f0790e019f266bc87eefe03dc52dad816d6952
chanel50 <106768673+Chanel50@users.noreply.github.com>
2023-09-17 00:09:07 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,58 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // Create a Set to keep track of seen numbers
+  const seenNumbers = new Set();
+
+  // Iterate through the array of numbers
+  for (const number of array) {
+    // Calculate the complement needed to reach the target
+    const complement = target - number;
+
+    // Check if the Set includes the complement
+    if (seenNumbers.has(complement)) {
+      return true; // If found, return true
+    }
+
+    // Add the current number to the Set
+    seenNumbers.add(number);
+  }
+
+  // If no pair is found, return false
+  return false;
 }
+
+// Test cases
+if (require.main === module) {
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("");
+  // Negative numbers?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+  // Multiple pairs?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+
+  console.log("");
+  // Single numbers?
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
+}
+
 
 /* 
   Write the Big O time complexity of your function here
